4b440cf231206895b6f0000ec96ee845ca715e4b
MaiHongPhong1902 <maihongphong.study@gmail.com>
2024-06-29 20:07:16 +0700

    upload v1 code

---
+++ script.js
@@ -0,0 +1,37 @@
+// Bat tat search bar
+function toggleSearchBar() {
+    var searchBar = document.getElementById('search-bar');
+    if (searchBar.classList.contains('visible')) {
+        searchBar.classList.remove('visible');
+        setTimeout(function() {
+            searchBar.style.display = 'none';
+        }, 500); // Thời gian khớp với transition
+    } else {
+        searchBar.style.display = 'flex';
+        setTimeout(function() {
+            searchBar.classList.add('visible');
+        }, 10); // Một chút thời gian để đảm bảo display đã được thiết lập
+    }
+}
+
+// Bat tat menu mobile
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+    var icon = document.getElementById('menu-mb');
+    if (icon.classList.contains('ti-menu')) {
+        icon.classList.add('icon-hidden'); // Ẩn icon hiện tại
+        setTimeout(function() {
+            icon.classList.remove('ti-menu');
+            icon.classList.remove('icon-hidden'); // Hiển thị icon mới
+            icon.classList.add('ti-close');
+        }, 200); // Đợi 0.5s để hiệu ứng ẩn hoàn tất
+    } else {
+        icon.classList.add('icon-hidden'); // Ẩn icon hiện tại
+        setTimeout(function() {
+            icon.classList.remove('ti-close');
+            icon.classList.remove('icon-hidden'); // Hiển thị icon mới
+            icon.classList.add('ti-menu');
+        }, 200); // Đợi 0.5s để hiệu ứng ẩn hoàn tất
+    }
+}
